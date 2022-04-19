a68f6871653cc442ed9fb0d02ff7eae7445b0f8f
JonasGN <j.goncalvesneto@outlook.com>
2022-04-19 12:31:15 -0300

    [add] responsiveness and details animations

--- js/main.js
+++ js/main.js
@@ -3,6 +3,10 @@ const portfolio = document.querySelector(".portfolio-slider");
 const portfolioItems = portfolio.querySelectorAll(".portfolio-item a");
 
 function addMarginTop() {
+  const windowSize = window.innerWidth;
+
+  if (windowSize <= 600) return;
+
   portfolioItems.forEach((item, index) => {
     item.style.marginTop = `${index * 5}rem`;
   });
